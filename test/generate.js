987e0e41a80e20a6348db06c289e111a3997d02d
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-19 19:09:48 +0200

    generate less nonsense

--- test/generate.js
+++ test/generate.js
@@ -1,15 +1,14 @@
-var levelup = require('levelup'),
-    uuid = require('node-uuid'),
+var uuid = require('node-uuid'),
     os    = require('os');
- 
+
 module.exports = function (db, cb) {
- 
+
     var length = 100 ;//ipsum.length;
- 
+
     function getRandomInt (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
- 
+
     function getIpsum(min,max) {
         return 'lorem ipsum\nlorem ipsum\nlorem ipsum\nlorem ipsum\n'
           + 'lorem ipsum\nlorem ipsum\n'
@@ -20,17 +19,17 @@ module.exports = function (db, cb) {
         }
         return result.join(" ");*/
     }
- 
- 
- 
+
+
+
     function randomDate(start, end) {
         return new Date(start + Math.ceil(Math.random() * (end - start))).toISOString();
     }
- 
+
     console.log('creating data');
     var posts = [];
     var startDate = new Date(2010, 0, 1).getTime(), endDate = new Date().getTime();
-    for(var i = 0; i< 20000; i++) {
+    for(var i = 0; i< 2000; i++) {
         posts[i] = {
             type : 'put',
             key : uuid.v4(),
@@ -41,16 +40,16 @@ module.exports = function (db, cb) {
             }
         }
     }
- 
+
     console.log("inserting data");
     var t = (new Date().getTime());
- 
+
     db.batch(posts, function (err) {
         if (err) return console.log('Ooops!', err)
         console.log('Great success dear leader!')
         cb()
     })
- 
+
     console.log('Data inserted in :' + ((new Date().getTime()) - t))
 
 }
